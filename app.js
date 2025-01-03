0938d5d33b6dbd0e665ae20e0811daadad0d215a
Brajbhushan3107 <24je0016@iitism.ac.in>
2025-01-03 22:08:17 +0530

    changes

--- app.js
+++ app.js
@@ -57,9 +57,9 @@ function submitQuiz() {
     const answers = {
         q1: document.querySelector('input[name="q1"]:checked').value,
         q2: document.querySelector('input[name="q2"]:checked').value,
-        q3: document.querySelector('input[name="q2"]:checked').value,
-        q4: document.querySelector('input[name="q2"]:checked').value,
-        q5: document.querySelector('input[name="q2"]:checked').value,
+        q3: document.querySelector('input[name="q3"]:checked').value,
+        q4: document.querySelector('input[name="q4"]:checked').value,
+        q5: document.querySelector('input[name="q5"]:checked').value,
 
     };
     let score = 0;
